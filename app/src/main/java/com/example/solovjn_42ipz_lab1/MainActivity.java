81f3ad967fdc16d1d0a13e2caa43275140e6aafd
s010m0n <olegsolovjn@gmail.com>
2019-04-24 01:26:32 +0300

    ііі

--- app/src/main/java/com/example/solovjn_42ipz_lab1/MainActivity.java
+++ app/src/main/java/com/example/solovjn_42ipz_lab1/MainActivity.java
@@ -13,18 +13,18 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-         spinner = (Spinner) findViewById(R.id.spinner);
-         textView = (TextView)findViewById(R.id.text);
+             spinner = (Spinner) findViewById(R.id.spinner);
+             textView = (TextView)findViewById(R.id.text);
 
-    }
+        }
     public void onClick(View view)
-    {
-        int position = spinner.getSelectedItemPosition();
-        String[] data2 = getResources().getStringArray(R.array.data2);
-        textView.setText(data2[position]);
-    }
+        {
+            int position = spinner.getSelectedItemPosition();
+            String[] data2 = getResources().getStringArray(R.array.data2);
+            textView.setText(data2[position]);
+        }
 }
